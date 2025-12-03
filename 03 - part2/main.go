dfdc11da407aea8d4a4a1b20e25808f8de00f2b3
José Eduardo <58561941+duduvillar10@users.noreply.github.com>
2025-12-03 11:23:04 -0300

    rename variables

--- 03 - part2/main.go
+++ 03 - part2/main.go
@@ -15,32 +15,32 @@ func main() {
 		panic(err)
 	}
 
-	groupBatteries := []string{}
+	banks := []string{}
 	if len(values) > 0 {
-		groupBatteries = strings.Split(strings.TrimSpace(string(values)), "\n")
+		banks = strings.Split(strings.TrimSpace(string(values)), "\n")
 	}
 
 	total := 0
 
-	for _, batteries := range groupBatteries {
-		pivotBattery := batteries[0:batteryCapacity]
+	for _, bank := range banks {
+		pivotBank := bank[0:batteryCapacity]
 
-		for i := batteryCapacity; i < len(batteries); i++ {
-			value := convertToInt(string(batteries[i]))
-			lastPivot := convertToInt(string(pivotBattery[batteryCapacity-1]))
+		for i := batteryCapacity; i < len(bank); i++ {
+			value := convertToInt(string(bank[i]))
+			lastFromPivot := convertToInt(string(pivotBank[batteryCapacity-1]))
 
-			newBatteries := removeLowestBatteryFrontToBack(pivotBattery)
-			if newBatteries != pivotBattery {
-				pivotBattery = newBatteries + string(batteries[i])
+			newBank := removeLowestBatteryFrontToBack(pivotBank)
+			if newBank != pivotBank {
+				pivotBank = newBank + string(bank[i])
 				continue
 			}
 
-			if value > lastPivot {
-				pivotBattery = removeLowestBatteryBackToFront(pivotBattery) + string(batteries[i])
+			if value > lastFromPivot {
+				pivotBank = removeLowestBatteryBackToFront(pivotBank) + string(bank[i])
 			}
 		}
 
-		value, err := strconv.Atoi(pivotBattery)
+		value, err := strconv.Atoi(pivotBank)
 		if err != nil {
 			panic(err)
 		}
@@ -51,30 +51,30 @@ func main() {
 	println("p1:", total)
 }
 
-func removeLowestBatteryBackToFront(batteries string) string {
-	for i := len(batteries) - 1; i > 0; i-- {
-		current := convertToInt(string(batteries[i]))
-		previous := convertToInt(string(batteries[i-1]))
+func removeLowestBatteryBackToFront(bank string) string {
+	for i := len(bank) - 1; i > 0; i-- {
+		current := convertToInt(string(bank[i]))
+		previous := convertToInt(string(bank[i-1]))
 
 		if current < previous {
-			return batteries[0:i] + batteries[i+1:]
+			return bank[0:i] + bank[i+1:]
 		}
 	}
 
-	return batteries[0 : len(batteries)-1]
+	return bank[0 : len(bank)-1]
 }
 
-func removeLowestBatteryFrontToBack(batteries string) string {
-	for i := 0; i < len(batteries)-1; i++ {
-		current := convertToInt(string(batteries[i]))
-		next := convertToInt(string(batteries[i+1]))
+func removeLowestBatteryFrontToBack(bank string) string {
+	for i := 0; i < len(bank)-1; i++ {
+		current := convertToInt(string(bank[i]))
+		next := convertToInt(string(bank[i+1]))
 
 		if current < next {
-			return batteries[0:i] + batteries[i+1:]
+			return bank[0:i] + bank[i+1:]
 		}
 	}
 
-	return batteries
+	return bank
 }
 
 func convertToInt(s string) int {
